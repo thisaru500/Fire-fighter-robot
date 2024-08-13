fed4f23bfd0149bf46990ea215ba4574f3ebc806
Thisaru Dissanayake <thisarudissanayake123@gmail.com>
2024-08-14 00:57:46 +0530

    define main pins of my project

---
+++ teeka_ayiya_s_project/teeka_ayiya_s_project.ino
@@ -0,0 +1,21 @@
+#define enA 10//Enable1 L298 Pin enA
+
+#define in1 9 //Motor1 L298 Pin in1
+
+#define in2 8 //Motor1 L298 Pin in2
+
+#define in3 7 //Motor2 L298 Pin in3
+
+#define in4 6 //Motor2 L298 Pin in4
+
+#define enB 5 //Enable2 L298 Pin enB
+
+#define ir_R A0
+
+#define ir_F A1
+
+#define ir_L A2
+
+#define servo A4
+
+#define pump A5
