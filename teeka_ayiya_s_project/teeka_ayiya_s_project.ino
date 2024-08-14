239d2d430dbd918a2dee0d64eb2786ffaaf3c108
Thisaru Dissanayake <thisarudissanayake123@gmail.com>
2024-08-14 09:28:39 +0530

    Declaration pins

--- teeka_ayiya_s_project/teeka_ayiya_s_project.ino
+++ teeka_ayiya_s_project/teeka_ayiya_s_project.ino
@@ -10,12 +10,30 @@
 
 #define enB 5 //Enable2 L298 Pin enB
 
-#define ir_R A0
+#define ir_R A0 //this sensor for getting distance for fire A0 A1 A2
 
 #define ir_F A1
 
 #define ir_L A2
 
-#define servo A4
+#define servo A4 //TO getting servo motor values
 
-#define pump A5
+#define pump A5 
+
+int Speed = 160; // Write The Duty Cycle 0 to 255 Enable for Motor Speed
+int s1, s2, s3;
+void setup(){ // put your setup code here, to run once
+Serial.begin(9600); // start serial communication at 9600bps
+pinMode(ir_R, INPUT);// declare fire sensor pin as input
+pinMode(ir_F, INPUT);// declare fire sensor pin as input
+pinMode(ir_L, INPUT);// declare fire sensor pin as input
+pinMode(enA, OUTPUT); // declare as output for L298 Pin enA
+pinMode(in1, OUTPUT); // declare as output for L298 Pin in1
+pinMode(in2, OUTPUT); // declare as output for L298 Pin in2
+pinMode(in3, OUTPUT); // declare as output for L298 Pin in3
+pinMode(in4, OUTPUT); // declare as output for L298 Pin in4
+pinMode(enB, OUTPUT); // declare as output for L298 Pin enB
+pinMode(servo, OUTPUT);
+pinMode(pump, OUTPUT);
+for (int angle = 90; angle <= 140; angle += 5) {
+servoPulse(servo, angle); }
